272ab43150f1e114a103a6e0c3099341a257f0c2
Vilazza1 <vinicius.vilaca2023@gmail.com>
2025-09-16 11:34:42 -0300

    feat: Initialize Next.js project with TypeScript, ESLint, and Tailwind CSS. Add configuration files, global styles, and a basic homepage component.

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+export function HomePage() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div>
+            <h1 className="text-5xl font-bold">ISUZU TRIPETCH</h1>
+            <h2 className="text-4xl">Make anything possible</h2>
+            <button className="text-base bg-[var(--primary-red)] text-white rounded-xl w-52 h-11">Discover Our Company</button>
+        </div>
+      </div>
+    );
+  }
