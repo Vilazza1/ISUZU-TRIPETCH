f0242777955e25b8c2c5459eb9649b361dfb5958
Vilazza1 <vinicius.vilaca2023@gmail.com>
2025-09-16 12:38:38 -0300

    feat: Add Banner SVG and update HomePage layout with background image. Fix CSS variable declaration by adding a semicolon.

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,12 @@
 export function HomePage() {
     return (
-      <div className="flex items-center justify-center h-screen">
-        <div>
-            <h1 className="text-5xl font-bold">ISUZU TRIPETCH</h1>
-            <h2 className="text-4xl">Make anything possible</h2>
-            <button className="text-base bg-[var(--primary-red)] text-white rounded-xl w-52 h-11">Discover Our Company</button>
+        <div className="flex items-center justify-center h-screen bg-cover bg-center bg-[url('/images/Banner.svg')]">
+        <div className="flex flex-col items-center">
+          <h1 className="text-5xl text-white font-bold mb-2">ISUZU TRIPETCH</h1> 
+          <h2 className="text-4xl text-white mb-10">Make anything possible</h2> 
+          <button className="text-base bg-[var(--primary-red)] text-white rounded-3xl w-52 h-11">
+            Discover Our Company
+          </button>
         </div>
       </div>
     );
