a28b102a2dbd36143eaddeba92064080d6e9b08d
Vilazza1 <vinicius.vilaca2023@gmail.com>
2025-11-25 08:46:04 -0300

    feat: integrate Carrosel component into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import {HomePage} from "@/components/HomePage";
 import Image from "next/image";
 import NavButton from "@/components/NavButton";
 import Cards from "@/components/Cards";
+import { Carrosel } from "@/components/Carrosel";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HomePage />
       <NavButton />
       <Cards />
+      <Carrosel />
     </div>
   );
 }
---
+++ src/components/Carrosel/index.tsx
@@ -0,0 +1,15 @@
+export function Carrosel() {
+
+    const images = [
+        { title: "Mission", src: "/images/Mission.png" },
+        { title: "People", src: "/images/People.png" },
+        { title: "Career", src: "/images/Career.png" },
+        { title: "Contact", src: "/images/Contact.png" },
+    ];
+
+    return (
+        <div>
+            <h1>Carrosel</h1>
+        </div>
+    );
+}
